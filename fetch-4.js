29be89f7b6061c562715ead546b74c03c9796b22
jlvFlores <jluisvazquez.jv@gmail.com>
2022-09-22 22:26:23 -0500

    24. Fetch Blob

---
+++ fetch-4.js
@@ -0,0 +1,12 @@
+
+
+
+fetch( 'superman.png' )
+    .then( resp => resp.blob() )
+    .then( imagen => {
+        
+        // console.log(imagen);
+        var imgPath = URL.createObjectURL( imagen );
+        img.src = imgPath;
+
+    });
